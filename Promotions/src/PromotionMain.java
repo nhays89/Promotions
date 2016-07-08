9d49e3076f78af6caf02844f627ae91a870688fb
nicholas hays <nicholas.hays@hotmail.com>
2016-07-08 15:00:59 -0700

    completed tree heirarchy. need to sort on node levels in hash map to get accurate count of employees that are within intervals and those that are not.

--- Promotions/src/PromotionMain.java
+++ Promotions/src/PromotionMain.java
@@ -4,15 +4,19 @@
  */
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class PromotionMain {
 
 	static int lowerInterval, higherInterval, numOfNodes, numOfRules;
-	static Map<String, Node> myTree = new HashMap<String, Node>();
+	static Map<String, Node> myTree = new TreeMap<String, Node>();
+	static ArrayList<Integer> myLevels = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		Scanner scanIn = new Scanner(System.in);
@@ -29,27 +33,46 @@ public class PromotionMain {
 			update(childNode);
 		}
 		scanIn.close();
-
+		for(Node n : myTree.values()) {
+			System.out.println("id " + n.myId + " level " + n.myLevel);
+		}
+	
 	}
+
 	static void createNodes() {
-		for(int i =0; i < numOfNodes; i++) {
+		for (int i = 0; i < numOfNodes; i++) {
 			Node node = new Node(String.valueOf(i));
 			myTree.put(node.myId, node);
 		}
 	}
-	
+
 	static void update(Node theChildNode) {
-		for(Node parentNode : theChildNode.myParentNodes) {
-			if(parentNode.myLevel + 1 > theChildNode.myLevel) {
+		for (Node parentNode : theChildNode.myParentNodes) {
+			if (parentNode.myLevel + 1 > theChildNode.myLevel) {
 				theChildNode.myLevel = parentNode.myLevel + 1;
-				for(Node ancestor : theChildNode.myChildNodes) {
-					update(ancestor);
+				for (Node child : theChildNode.myChildNodes) {
+					update(child);
 				}
+				break;
 			}
 		}
 	}
 
-	public static class Node {
+	static int determinePromotions(int promotions) {
+		int maxPromoted = 0;
+		for (int i = 0; i < myLevels.size(); i++) {
+			if (promotions - ((int) myLevels.get(i)) > 0) {
+				maxPromoted += ((int) myLevels.get(i));
+			} else if ((promotions - ((int) myLevels.get(i))) == 0) {
+				return maxPromoted + ((int) myLevels.get(i));
+			} else {
+				return maxPromoted;
+			}
+		}
+		return maxPromoted;
+	}
+
+	public static class Node implements Comparable<Node> {
 
 		String myId;
 		List<Node> myParentNodes = new ArrayList<Node>();
@@ -57,8 +80,18 @@ public class PromotionMain {
 		int myLevel;
 
 		public Node(String theId) {
-			myLevel++;
 			myId = theId;
+		}
+
+		@Override
+		public int compareTo(Node node) {
+			if (this.myLevel > node.myLevel) {
+				return -1;
+			} else if (this.myLevel == node.myLevel) {
+				return 0;
+			} else {
+				return 1;
+			}
 		}
 	}
 
